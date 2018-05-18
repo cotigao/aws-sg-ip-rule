fc884eee165e1bcbdd7a1da8effdfba1638b264a
Vikram Fugro <vikram.fugro@gmail.com>
2018-05-18 08:48:23 +0530

    wrapperscript to to handle multiple rules

---
+++ coverfire.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function usage {
+    echo "$0 u -n <rul1file1> <rulefile2> ... "
+    echo "$0 a -n <rul1file1> <rulefile2> ... "
+    echo "$0 r -n <rul1file1> <rulefile2> ... "
+    exit 1
+}
+
+[ "$1" = "u" ] || [ "$1" = "a" ] || [ "$1" = "r" ] || usage
+op="$1"
+shift
+
+[ "$1" = "-n" ] || usage
+shift
+
+for x in $@; do
+    ./aws-sg-ip-rule.sh "$op" -n "$x"
+done
